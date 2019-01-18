fab8e35555271cc5076bf3d8183b3f8e673822c7
Maarten de Keizer <github@maartendekeizer.nl>
2019-01-18 12:34:33 +0100

    Remove debug

--- app/write-keys.sh
+++ app/write-keys.sh
@@ -8,12 +8,6 @@ mkdir /keys
 echo "$GPG_PRIVATE_BASE64" | base64 -d > /keys/private.gpg
 echo "$GPG_PUBLIC_BASE64" | base64 -d > /keys/public.asc
 
-echo "Debug show public key"
-echo "$GPG_PUBLIC_BASE64"
-echo "Debug show private key"
-echo "$GPG_PRIVATE_BASE64"
-echo "$GPG_PRIVATE_BASE64" | base64
-
 ls -larth /keys
 
 echo ""
