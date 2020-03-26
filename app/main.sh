db1091fa376c446f368d59b9b515be44fcceb13d
Dave Neijsen <dave@focusautomation.nl>
2020-03-26 12:20:19 +0100

    Disable upload to Decos in acceptance

--- app/main.sh
+++ app/main.sh
@@ -1,8 +1,27 @@
 #!/bin/bash
 
+ENVIRONMENT=$1
+
+print_help() {
+  echo "Usage: $0 [acceptance|production]"
+  echo "When running 'acceptance', the upload to Decos is disabled"
+  exit 1
+}
+
+if [ -z $ENVIRONMENT ]; then
+  print_help
+fi
+if [ "$ENVIRONMENT" != "acceptance" ] && [ "$ENVIRONMENT" != "production" ]; then
+  print_help
+fi
+
+echo "Running script for environment '$ENVIRONMENT'"
+
 ./write-keys.sh
 ./download-from-objectstore.sh
 ./decrypt.sh
 ./upload-to-makkelijkemarkt.sh
-./upload-to-decos.sh
+if [ "$ENVIRONMENT" == "production" ]; then
+  ./upload-to-decos.sh
+fi
 ./clean.sh
