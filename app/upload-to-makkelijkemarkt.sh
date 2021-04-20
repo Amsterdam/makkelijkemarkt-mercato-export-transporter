fe77182961d44dda126625245dcf75d88bb9753b
Jos Houtman <jos@houtman.it>
2021-04-20 15:08:50 +0200

    Split-off upload and download objectstore

--- app/upload-to-makkelijkemarkt.sh
+++ app/upload-to-makkelijkemarkt.sh
@@ -6,15 +6,17 @@ echo "=====> Upload to MakkelijkeMarkt"
 echo "Switch workdir to /tmp/download"
 cd /tmp/download
 
+
+
 for file in *
 do
   swift \
     --auth-version 3 \
     --os-auth-url=$SWIFT_AUTH_URL \
     --os-region-name=$SWIFT_REGION \
-    --os-username=$SWIFT_USER_NAME \
-    --os-password=$SWIFT_USER_PASSWORD \
-    --os-project-id=$SWIFT_PROJECT_ID \
+    --os-username=$DST_SWIFT_USER_NAME \
+    --os-password=$DST_SWIFT_USER_PASSWORD \
+    --os-project-id=$DST_SWIFT_PROJECT_ID \
     --os-project-domain-id=$SWIFT_DOMAIN_ID \
     upload $MAKKELIJKEMARKT_CONTAINER $file
 done
@@ -27,9 +29,9 @@ swift \
   --auth-version 3 \
   --os-auth-url=$SWIFT_AUTH_URL \
   --os-region-name=$SWIFT_REGION \
-  --os-username=$SWIFT_USER_NAME \
-  --os-password=$SWIFT_USER_PASSWORD \
-  --os-project-id=$SWIFT_PROJECT_ID \
+  --os-username=$DST_SWIFT_USER_NAME \
+  --os-password=$DST_SWIFT_USER_PASSWORD \
+  --os-project-id=$DST_SWIFT_PROJECT_ID \
   --os-project-domain-id=$SWIFT_DOMAIN_ID \
   list $MAKKELIJKEMARKT_CONTAINER
 
